6d886612e374ced449cb89be461050a87645ffb3
Nitin Thakur <nitinthakur46323@gmail.com>
2024-08-06 13:43:53 +0530

    banner dummy

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import HomeProductCard from "../components/HomeProductCard";
 import TrendingNow from "../components/TrendingNow";
 import Slider from "../components/Slider/Slider";
+import Banner from "../components/Banner/Bannercards";
 
 const Home = () => {
   return (
     <div>
+      <Banner />
       <TrendingNow />
       <Slider />
       <HomeProductCard />
