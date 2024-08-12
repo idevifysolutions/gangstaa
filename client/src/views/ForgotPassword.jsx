0da329bec98bec6be39b5bdf6b45330707480930
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-08-12 10:55:59 +0530

    carousel images, eye icons added

--- client/src/views/ForgotPassword.jsx
+++ client/src/views/ForgotPassword.jsx
@@ -1,20 +1,24 @@
-// src/components/ForgotPassword.js
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { FaLock } from "react-icons/fa";
+import { FaLock, FaEye, FaEyeSlash } from "react-icons/fa";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+
+const images = [
+  "https://images.bewakoof.com/original/men-s-blue-den-printed-oversized-shirt-604442-1698919129-2.jpg",
+  "https://thehouseofrare.com/cdn/shop/files/HERO_7c109915-b917-440a-a14a-5fe90a7571ef_765x.jpg?v=1710235193",
+  "https://images.bewakoof.com/t1080/men-s-blue-black-run-checked-oversized-shirt-604430-1721135248-1.jpg",
+  "https://images.bewakoof.com/t1080/men-s-blue-den-graphic-printed-oversized-shirt-604442-1721131088-1.jpg",
+];
 
 const ForgotPassword = () => {
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const navitage = useNavigate();
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const navigate = useNavigate();
 
   const handleUpdatePassword = async () => {
-    // if (newPassword || confirmPassword === "") {
-    //   alert("Fill in both fields");
-    //   return;
-    // }
     if (newPassword !== confirmPassword) {
       alert("Passwords do not match");
       return;
@@ -25,7 +29,7 @@ const ForgotPassword = () => {
         { newPassword }
       );
       console.log(response.data);
-      navitage.push("/login");
+      navigate("/login");
       // Handle successful password update
     } catch (error) {
       console.error(error);
@@ -33,13 +37,51 @@ const ForgotPassword = () => {
     }
   };
 
+  const toggleNewPasswordVisibility = () => {
+    setShowNewPassword((prevShowNewPassword) => !prevShowNewPassword);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(
+      (prevShowConfirmPassword) => !prevShowConfirmPassword
+    );
+  };
+
   return (
-    <div className="flex items-center justify-center min-h-screen bg-white">
+    <div className="relative flex items-center justify-center h-screen bg-white overflow-hidden">
+      {/* Background Image Carousel */}
       <motion.div
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
-        className="p-6 bg-gray-100 shadow-md rounded-md w-full max-w-sm"
+        className="absolute inset-0 z-0"
+      >
+        <motion.div
+          initial={{ x: "0%" }}
+          animate={{ x: "-100%" }}
+          transition={{
+            duration: 50,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "loop",
+          }}
+          className="flex w-[400%] h-full"
+        >
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className="w-full h-full bg-cover bg-center"
+              style={{ backgroundImage: `url(${image})`, filter: "blur(3px)" }}
+            />
+          ))}
+        </motion.div>
+      </motion.div>
+      <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        className="relative z-20 p-6 bg-gray-100 shadow-md rounded-md w-full max-w-sm"
       >
         <h2 className="text-2xl font-semibold text-center text-black mb-4">
           Reset Password
@@ -48,32 +90,44 @@ const ForgotPassword = () => {
           <label className="block text-black mb-2" htmlFor="newPassword">
             New Password
           </label>
-          <div className="flex items-center border border-gray-400 rounded-md">
+          <div className="flex items-center border border-gray-400 rounded-md relative">
             <FaLock className="ml-2 text-gray-600" />
             <input
-              type="password"
+              type={showNewPassword ? "text" : "password"}
               id="newPassword"
               value={newPassword}
               onChange={(e) => setNewPassword(e.target.value)}
               className="w-full px-3 py-2 text-black bg-white border-none rounded-md focus:outline-none"
               placeholder="Enter your new password"
             />
+            <div
+              onClick={toggleNewPasswordVisibility}
+              className="absolute right-3 cursor-pointer text-gray-600"
+            >
+              {showNewPassword ? <FaEyeSlash /> : <FaEye />}
+            </div>
           </div>
         </div>
         <div className="mb-4">
           <label className="block text-black mb-2" htmlFor="confirmPassword">
             Confirm Password
           </label>
-          <div className="flex items-center border border-gray-400 rounded-md">
+          <div className="flex items-center border border-gray-400 rounded-md relative">
             <FaLock className="ml-2 text-gray-600" />
             <input
-              type="password"
+              type={showConfirmPassword ? "text" : "password"}
               id="confirmPassword"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
               className="w-full px-3 py-2 text-black bg-white border-none rounded-md focus:outline-none"
               placeholder="Confirm your new password"
             />
+            <div
+              onClick={toggleConfirmPasswordVisibility}
+              className="absolute right-3 cursor-pointer text-gray-600"
+            >
+              {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+            </div>
           </div>
         </div>
         <button
