5512b1362aaf3f5f0dcdc1ae0ae68e866c15cc94
Avinash <avinashghanghav3d@gmail.com>
2024-09-02 13:44:07 +0530

    maked responsive cartamt page

--- client/src/views/Cart.jsx
+++ client/src/views/Cart.jsx
@@ -29,12 +29,10 @@ const Cart = () => {
     dispatch(removeProductFromCart(productId));
   };
 
-
-  useEffect(()=>{
-    dispatch(calculatePrice())
+  useEffect(() => {
+    dispatch(calculatePrice());
     localStorage.setItem("cart_items", JSON.stringify(cartItems));
-  }, [cartItems])
-
+  }, [cartItems]);
 
   return (
     <div className='py-8 px-16 flex flex-wrap justify-center gap-[4rem] "h-[calc(100vh-4rem)]"'>
@@ -42,12 +40,13 @@ const Cart = () => {
         {cartItems.length > 0 ? (
           cartItems.map((item, index) => (
             <>
-
-
-              <CartItemCard key={crypto.randomUUID()} cartItem={item} incrementHandler={incrementHandler} decrementHandler={decrementHandler} removeHandler={removeHandler} />
-
-
-
+              <CartItemCard
+                key={crypto.randomUUID()}
+                cartItem={item}
+                incrementHandler={incrementHandler}
+                decrementHandler={decrementHandler}
+                removeHandler={removeHandler}
+              />
             </>
           ))
         ) : (
@@ -56,39 +55,37 @@ const Cart = () => {
           </div>
         )}
       </main>
-      <aside className="w-[80%] md:w-[30%] lg:w-[30%] h-[70%] p-16 flex flex-col justify-center items-stretch gap-[1.3rem] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
-        <p className="text-[1.1rem] flex justify-between">
+      <aside className="w-[80%] md:w-[30%] lg:w-[30%] h-[70%] p-4 md:p-8 lg:p-16 flex flex-col justify-center items-stretch gap-[1rem] md:gap-[1.3rem] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+        <p className="text-base md:text-lg lg:text-xl flex justify-between">
           Subtotal: <span>₹ {subtotal}</span>
         </p>
-        <p className="text-[1.1rem] flex justify-between">
+        <p className="text-base md:text-lg lg:text-xl flex justify-between">
           Shipping Charges: <span>₹ {shippingCharges}</span>
         </p>
-        <p className="text-[1.1rem] flex justify-between">
+        <p className="text-base md:text-lg lg:text-xl flex justify-between">
           Tax: <span>₹ {tax}</span>
         </p>
-        <p className="text-[1.1rem] flex justify-between">
+        <p className="text-base md:text-lg lg:text-xl flex justify-between">
           Discount: <span>₹ {discount}</span>
         </p>
-       {
-        subtotal === 0 ? ( <p className="text-[1.1rem] font-bold flex justify-between">
-          Total: <span>₹ {0}</span>
-        </p>): ( <p className="text-[1.1rem] font-bold flex justify-between">
-          Total: <span>₹ {total}</span>
-        </p>)
-       }
+        {subtotal === 0 ? (
+          <p className="text-base md:text-lg lg:text-xl font-bold flex justify-between">
+            Total: <span>₹ {0}</span>
+          </p>
+        ) : (
+          <p className="text-base md:text-lg lg:text-xl font-bold flex justify-between">
+            Total: <span>₹ {total}</span>
+          </p>
+        )}
 
-        {
-          //Add the route to the shipping form page
-          cartItems.length > 0 && (
-            <Link
-              className="bg-gray-600 text-white 
-          text-2xl text-center rounded-md p-3 hover:bg-gray-500 "
-              to="/shipping"
-            >
-              Checkout
-            </Link>
-          )
-        }
+        {cartItems.length > 0 && (
+          <Link
+            className="bg-gray-600 text-white text-base md:text-lg lg:text-xl text-center rounded-md p-2 md:p-3 lg:p-4 hover:bg-gray-500"
+            to="/shipping"
+          >
+            Checkout
+          </Link>
+        )}
       </aside>
     </div>
   );
